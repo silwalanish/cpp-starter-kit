e10c8eab86433ef2e57d40ef3c12d40288749fe6
silwalanish <silwalanish@gmail.com>
2020-08-01 10:41:04 +0545

    Add documentation for LINK_PARAMS

--- BuildOptions.mk
+++ BuildOptions.mk
@@ -6,6 +6,9 @@ EXECUTABLE_NAME ?= $(PROJECT_NAME).o
 # Add custom include paths
 ADDITIONAL_INCLUDES = -I$(VENDOR_DIR)/glm
 
-# SET CCFLAGS and CFLAGS for more params for compiler and linker
+# SET CCFLAGS and CFLAGS for more params for compiler
 # CCFLAGS=
 # CFLAGS=
+
+# SET LINK_PARAMS for more params for linker
+# LINK_PARAMS=
